ded4b9f92cd5b0d3b8b95f2744b11c68185c9b6f
Ben Langlois <blanglois000@gmail.com>
2017-11-29 16:38:42 -0500

    Wrote majority of program, has not been tested yet.

--- lab5/lab5.ino
+++ lab5/lab5.ino
@@ -1,17 +1,70 @@
-int incomingByte = 0;
+int input;
+bool receivedInput = false;
+unsigned long startTime;
+
+#define MOTOR1PWM 5
+#define MOTOR1DIR 7
+#define MOTOR1BRK 12
+
+
+#define MOTOR2PWM 6
+#define MOTOR2DIR 8
+#define MOTOR1BRK 13
+
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // Set pins
+  pinMode(MOTOR1PWM, OUTPUT);
+  pinMode(MOTOR2PWM, OUTPUT);
+  pinMode(MOTOR1DIR, OUTPUT);
+  pinMode(MOTOR2DIR, OUTPUT);
+  pinMode(MOTOR1BRK, OUTPUT);
+  pinMode(MOTOR2BRK, OUTPUT);
+
+
+  digitalWrite(MOTOR1DIR, HIGH);
+  digitalWrite(MOROR2DIR, HIGH);
+  digitalWrite(MOTOR1BRK, HIGH);
+  digitalWrite(MOROR2BRK, HIGH);
+  digitalWrite(MOTOR1PWM, 0);
+  digitalWrite(MOROR2PWM, 0);
+
+  
+  Serial.println("Enter a motor speed!");
 } 
 
-void loop() {
-  if (Serial.available() > 0) { //https://www.arduino.cc/en/Serial/Read
-      // read the incoming byte:
-      incomingByte = Serial.read();
 
-      // say what you got:
-      Serial.print("I received: ");
-      Serial.println(incomingByte, DEC);
-   }
+void loop() {
+  if (receivedInput == false) { // Take input from user
+    if (Serial.available() > 0) {
+      input = (Serial.readString()).toInt();
+
+      if (0 <= input <= 255) Serial.println("Enter a valid integer between 0 and 255.");
+      else {
+        Serial.println("Running motors!");
+        receivedInput = true;
+        
+        digitalWrite(MOTOR1BRK, LOW);
+        digitalWrite(MOROR2BRK, LOW);
+        
+        analogWrite(MOTOR1PWM, input);
+        analogWrite(MOTOR2PWM, input);
+        startTime = millis();
+      }
+    }
+    
+  } else { // Run motor for 10 seconds
+    if (millis() > startTime + 10000) { // 10 seconds has passed
+      recievedInput = false;
+      Serial.println("Enter a new motor speed!");
+        
+      analogWrite(MOTOR1PWM, 0);
+      analogWrite(MOTOR2PWM, 0);
+      
+      digitalWrite(MOTOR1BRK, HIGH);
+      digitalWrite(MOROR2BRK, HIGH);
+    }
+  }
 }
