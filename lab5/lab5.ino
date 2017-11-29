b43e1f91b3475ca61a2e9b913f7e588f187f008b
Jayden Lam <jk2999la@edu.uwaterloo.ca>
2017-11-29 16:00:46 -0500

    test code

--- lab5/lab5.ino
+++ lab5/lab5.ino
@@ -1,10 +1,17 @@
+int incomingByte = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Hello, World!");
 } 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  if (Serial.available() > 0) { //https://www.arduino.cc/en/Serial/Read
+      // read the incoming byte:
+      incomingByte = Serial.read();
+
+      // say what you got:
+      Serial.print("I received: ");
+      Serial.println(incomingByte, DEC);
+   }
 }
